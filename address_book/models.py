090a41a8684ebd601310fd2ea5edf8a9d82859a2
Veiko <veiko.maesalu@gmail.com>
2020-08-31 17:41:42 +0300

    add address parandusega

--- address_book/models.py
+++ address_book/models.py
@@ -6,5 +6,5 @@ class Address(models.Model):
 	address = models.CharField(max_length=200)
 	email = models.EmailField(max_length=200)
 
-def __str__(self):
-	return self.name
+	def __str__(self):
+		return self.name
