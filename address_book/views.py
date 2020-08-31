1be41cafbfb59745ca01614835e1ab558754570e
Veiko <veiko.maesalu@gmail.com>
2020-08-31 18:17:36 +0300

    aadresside lisamine veebilehele

--- address_book/views.py
+++ address_book/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from .models import Address
 
 def home(request):
-	return render(request, 'home.html', {})
+	all_addresses = Address.objects.all
+	return render(request, 'home.html', {'all_addresses': all_addresses})
 
 def add_address(request):
 	return render(request, 'add_address.html', {})
