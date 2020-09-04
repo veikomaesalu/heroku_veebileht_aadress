9cdaf1ba8b4d80585318b54fbc12a3c30e13004f
Veiko <veiko.maesalu@gmail.com>
2020-09-04 18:58:54 +0300

    kõik valmis

--- address_book/urls.py
+++ address_book/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('', views.add_address, name="add_address"),
+    path('add_address/', views.add_address, name="add_address"),
+    path('edit/<list_id>', views.edit, name="edit"),
+    path('delete/<list_id>', views.delete, name="delete"),
 ]
--- address_book/views.py
+++ address_book/views.py
@@ -1,9 +1,48 @@
 from django.shortcuts import render, redirect
 from .models import Address
+from .forms import AddressForm
+from django.contrib import messages
+
 
 def home(request):
 	all_addresses = Address.objects.all
 	return render(request, 'home.html', {'all_addresses': all_addresses})
 
 def add_address(request):
-	return render(request, 'add_address.html', {})
+	if request.method=="POST":
+		form = AddressForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Address has been added!'))
+			return redirect('home')
+		else:
+			messages.success(request, ('Seems like there was an error...'))
+			return render(request, 'add_address.html', {})
+	else:
+		return render(request, 'add_address.html', {})
+
+def edit(request, list_id):
+	if request.method=='POST':
+		current_address = Address.objects.get(pk=list_id)
+		form = AddressForm(request.POST or None, instance=current_address)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Address has been edited!'))
+			return redirect('home')
+		else:
+			messages.success(request, ('Seems like there was an error...'))
+			return render(request, 'edit.html', {})
+	else:
+		get_address = Address.objects.get(pk=list_id)
+		return render(request, 'edit.html', {'get_address': get_address})
+
+def delete(request, list_id):
+	if request.method=='POST':
+		current_address = Address.objects.get(pk=list_id)
+		current_address.delete()
+		messages.success(request, ('Address has been deleted!'))
+		return redirect('home')
+	else:
+		messages.success(request, ('Nothing to see here...'))
+		return redirect('home')
+	
